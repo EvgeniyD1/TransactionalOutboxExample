3ae71aa19764f4aa4dd950a65b1a43a63cdc06fe
EvgeniyD1 <edolgiyj7@gmail.com>
2022-10-11 19:15:39 +0300

    1

--- order_service/src/main/java/com/example/order_service/domain/Outbox.java
+++ order_service/src/main/java/com/example/order_service/domain/Outbox.java
@@ -4,7 +4,6 @@ import com.example.order_service.converter.JsonToMapConverter;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
